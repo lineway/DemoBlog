71bc0c9fe41e55d2f9c5fa60d2d07b9dda24ce44
张一铭 <zhangyiming@Piels-MacBook-Pro.local>
2016-12-12 23:59:28 +0800

    建立User Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+# coding=utf-8
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatar/%Y/%m', default='avatar/default.png', max_length=200, blank=True, null=True,verbose_name='用户头像')
+    qq = models.CharField(max_length=20, blank=True, null=True, verbose_name='QQ号码')
+    mobile = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号码')
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+        ordering = ['-id']
+
+    def __unicode__(self):
+        return self.username
