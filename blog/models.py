efcea25eab4e63c6db504587bc156fce951ecabd
张一铭 <zhangyiming@Piels-MacBook-Pro.local>
2016-12-13 00:06:56 +0800

    建立Category Models

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,17 @@ class Tag(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+# 文章分类
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name='分类名称')
+    index = models.IntegerField('显示名称(从小到大)', default=999)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+        ordering = ['index', 'id']
+
+    def __unicode__(self):
+        return self.name
