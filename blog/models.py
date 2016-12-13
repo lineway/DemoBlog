6c4c1185bb74ec75e29007cdead349331d6dcde5
张一铭 <zhangyiming@Piels-MacBook-Pro.local>
2016-12-13 21:47:19 +0800

    建立Links Models

--- blog/models.py
+++ blog/models.py
@@ -53,7 +53,7 @@ class Article(models.Model):
     content = models.TextField(verbose_name='文章正文')
     click_count = models.IntegerField(default=0, verbose_name='点击次数')
     is_recommend = models.BooleanField(default=False, verbose_name='是否推荐')
-    data_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
     user = models.ForeignKey(User, verbose_name='用户')
     category = models.ForeignKey(Category, blank=True, null=True, verbose_name='分类')
     tag = models.ManyToManyField(Tag, verbose_name='标签')
@@ -82,4 +82,20 @@ class Comment(models.Model):
 
     def __unicode__(self):
         return str(self.id)
-    
+
+
+# 友情链接
+class Links(models.Model):
+    title = models.CharField(max_length=50, verbose_name='标题')
+    description = models.CharField(max_length=200, verbose_name='友情链接描述')
+    callback_url = models.URLField(verbose_name='url地址')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    index = models.IntegerField(default=999, verbose_name='排列顺序(从小到大)')
+
+    class Meta:
+        verbose_name = '友情链接'
+        verbose_name_plural = verbose_name
+        ordering = ['index', 'id']
+
+    def __unicode__(self):
+        return self.title
