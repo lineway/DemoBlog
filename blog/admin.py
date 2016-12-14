a3cadd2768376ad703ff29de457e6ea75d7bb35a
张一铭 <zhangyiming@Piels-MacBook-Pro.local>
2016-12-14 23:59:37 +0800

    简单配置admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,26 @@ from blog.models import *
 
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('title', 'desc', 'content')
+
+class ArticleAdmin(admin.ModelAdmin):
+    # fields = ('title', 'desc', 'content')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content', )
+        }),
+        ('高级设置', {
+            'classes': ('collapse',),
+            'fields': ('click_count', 'is_recommend', )
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(Links)
 admin.site.register(Ad)
