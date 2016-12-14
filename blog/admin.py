ceacea45fb14558f62cabb097d0d88a2e293e16f
张一铭 <zhangyiming@Piels-MacBook-Pro.local>
2016-12-14 23:00:14 +0800

    注册admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
+# coding:utf-8
 from django.contrib import admin
+from blog.models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
