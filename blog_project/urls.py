8a3c8378e070091bce043c8746760251a6313b68
zhangyiming <zhangyiming07@163.com>
2016-12-25 23:00:13 +0800

    配置上传url

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from blog.upload import upload_image
 from blog.views import index
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index, name='index'),
     url(r'^upload/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
+    url(r'^admin/upload/(?P<dir_name>[^/]+)$', upload_image, name='upload_image'),
 ]
